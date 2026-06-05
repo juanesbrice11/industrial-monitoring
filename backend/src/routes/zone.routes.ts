01e8000358655406fc94996ddd67046c56e732e3
juanesbrice11 <juanesbrice977@gmail.com>
2026-06-05 16:18:14 -0500

    feat: add GET /zones endpoint

--- backend/src/routes/zone.routes.ts
+++ backend/src/routes/zone.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { getActiveSensorsByZone } from '../controllers/zone.controller';
+import {
+  getAllZones,
+  getActiveSensorsByZone,
+} from '../controllers/zone.controller';
 
 const router = Router();
 
+router.get('/', getAllZones);
 router.get('/:id/sensors', getActiveSensorsByZone);
 
 export default router;
